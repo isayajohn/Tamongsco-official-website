b15deaf667dff17275f9e00d491b5463e7b1bc98
Isaya Kapama <isayajohn616@gmail.com>
2026-06-02 13:21:57 +0300

    Avoid admin route conflict in production

--- backend/tamongsco_backend/urls.py
+++ backend/tamongsco_backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
     path("api/", include("cms.urls")),
-    re_path(r"^(?!api/|admin/|static/).*$", TemplateView.as_view(template_name="index.html"), name="frontend"),
+    re_path(r"^(?!api/|django-admin/|static/).*$", TemplateView.as_view(template_name="index.html"), name="frontend"),
 ]
